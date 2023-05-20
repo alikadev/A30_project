70ae690fad2344bb0c59995e104610e073222a26
alikadev <dev@alikadev.com>
2023-05-20 13:32:57 +0200

    Add Geolocation and a Button in TT

--- TT/App.tsx
+++ TT/App.tsx
@@ -1,14 +1,45 @@
 import React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, View, Platform, Dimensions } from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
+import {
+  HStack,
+  Button,
+} from 'swiftui-react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <MapView style={styles.map}/>
-      <Text style={styles.item}>Hello, world!</Text>
-    </View>
-  );
+function getLocationPermission(success: any, failure: any): void
+{
+  try {
+    Geolocation.requestAuthorization('whenInUse')
+    .then(success)
+    .catch(failure)
+  } catch (error) {
+    console.warn(error)
+  }
+}
+
+function getLocation(success: any, failure: any): void
+{
+  try {
+    Geolocation.getCurrentPosition(success, failure)
+  } catch(error) {
+    console.log(error)
+  }
+}
+type Coord = {
+  longitude: number
+  latitude: number
+}
+
+const screen = Dimensions.get('window')
+const ASPECT_RATIO = screen.width / screen.height
+const LATITUDE_DELTA = 0.04
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
+
+
+function buttonIsPressed()
+{
+  console.log("Button has been pressed")
 }
 
 const styles = StyleSheet.create({
@@ -24,3 +55,35 @@ const styles = StyleSheet.create({
     flex: 3,
   },
 });
+
+let mapview = new MapView(
+  {
+    style: styles.map, 
+    region: {
+      latitude: 0, 
+      longitude: 0, 
+      latitudeDelta: LATITUDE_DELTA, 
+      longitudeDelta: LONGITUDE_DELTA}});
+export default function App() {
+
+  getLocationPermission(() => {
+    getLocation(async (data: any) => {
+      let lat = data.coords.latitude
+      let lon = data.coords.longitude
+      console.log(lat, lon)
+      mapview.animateToRegion({longitude: lon, latitude: lat, latitudeDelta: LATITUDE_DELTA, longitudeDelta: LONGITUDE_DELTA})
+    }, () => {
+    })
+  }, () => {
+    console.warn("Cannot get location permission")
+  })
+  
+  return (
+    <View style={styles.container}>
+      {mapview.render()}
+      <HStack style={styles.item}>
+        <Button title='MyButton'></Button>
+      </HStack>
+    </View>
+  )
+}
